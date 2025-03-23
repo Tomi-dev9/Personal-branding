b6756a3567cc4a149705451b81b80494c2ad17e4
Tomi-dev9 <tomingselingga2512@gmail.com>
2025-03-23 16:45:14 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import './App.css';
-import Header from './Header';
-import Education from './Education';
-import Projects from './Projects';
-import Skills from './Skills';
-import Footer from './Footer';
+import Header from './components/Header';
+import Education from './components/Education';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Footer from './components/Footer';
 
 function App() {
   return (
